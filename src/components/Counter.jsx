ee237947680ff0862c92a3907bf591d635e2b810
ACM-Anik <anikmojumder5252@gmail.com>
2024-02-14 19:43:54 +0600

    dispatch and playload implemented

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByValue } from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
 
     const { count } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
     console.log(count);
 
     return (
         <div>
             <h1 className="text-center">Counter</h1>
             <div>
-                <button>Increment</button>
-                <div>
+                <button onClick={() => dispatch(incrementByValue(5))} className="bg-blue-500 p-2">Increment By Value 5</button>
+                <br />
+                <br />
+
+                <button onClick={() => dispatch(increment())} className="bg-purple-500 p-2">Increment</button>
+                <div className="text-4xl">
                     <h1>{count}</h1>
                 </div>
-                <button>Decrement</button>
+                <button onClick={() => dispatch(decrement())} className="bg-purple-500 p-2">Decrement</button>
             </div>
         </div>
     )
