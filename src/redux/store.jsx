d71ed1af362625b968270e60bc1c1354552a9f68
ACM-Anik <anikmojumder5252@gmail.com>
2024-02-20 23:22:59 +0600

    Redux logger (thirdparty) implemented and also old custom redux logger(curring) implemented

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,14 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './features/counter/counterSlice';
 import cartSliceReducer from './features/cart/cartSlice';
-
+// import logger from 'redux-logger';
+// Note: Though we can use third-party logger but also can use redux old custom (curring type) middleware
+import logger from './middlewares/logger';
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         cart: cartSliceReducer,
     },
-    
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(logger),
 });
 
 export default store;
